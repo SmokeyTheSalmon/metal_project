2e9ace2e493de0d152a6bbfb16583cc4c3570852
SmokeytheSalmon <Roelvanbakkum@gmail.com>
2015-01-08 13:04:24 +0100

    Server now writes to data directory

--- server.js
+++ server.js
@@ -4,36 +4,26 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-var json    = {};
-
-function scrape(link) {
-  var $ = cheerio.load(link);
-  var scrapedtext = $('body').prevObject.text();
-  res.send(scrapedtext)
-  json += scrapedtext;  
-}
-
-function parseAndWrite() {
-  json = JSON.parse(json);
-  console.log(json);
-  fs.writeFile('data/' + country + '.json', JSON.stringify(json, null, 2), function(err) {
-    console.log("Mission succesful");
-  })
-}
-
 app.get('/scrape', function(req, res){
 
-  url = "http://www.metal-archives.com/browse/ajax-country/c/NL/json/1?sEcho=1&iDisplayStart=0";
+  var country = "NL";
+  url = "http://www.metal-archives.com/browse/ajax-country/c/" + country + "/json/1?sEcho=1&iDisplayStart=0";
 
   request(url, function(error, response, html) {
     
-
     if(!error){
-      scrape(html);
+      var $ = cheerio.load(html);
+      var scrapedtext = $('body').prevObject.text();
+      res.send(scrapedtext)
+      var json = scrapedtext;
 
-      parseAndWrite();
+      json = JSON.parse(json);
+      console.log(json);
+      fs.writeFile('data/' + country + '.json', JSON.stringify(json, null, 2), function(err) {
+        console.log("Mission succesful");
+        process.exit(0);
+      })
 
-      // process.exit(0);
     }
   })
 })
