d36c811be3f8dbe0a75c77791260a100070c8376
SmokeytheSalmon <Roelvanbakkum@gmail.com>
2015-01-08 15:18:02 +0100

    Try to extract more than one page

--- server.js
+++ server.js
@@ -7,25 +7,33 @@ var app     = express();
 app.get('/scrape', function(req, res){
 
   var country = "NL";
+  var data = {};
+  var rounds = 1;
+
   url = "http://www.metal-archives.com/browse/ajax-country/c/" + country + "/json/1?sEcho=1&iDisplayStart=0";
 
-  request(url, function(error, response, html) {
-    
+  request(url, function(error, response, html) {  
     if(!error){
       var $ = cheerio.load(html);
-      var scrapedtext = $('body').prevObject.text();
-      res.send(scrapedtext)
-      var json = scrapedtext;
+      var json = $('body').prevObject.text();
+      res.send("Scraped " + country + "!");
 
       json = JSON.parse(json);
-      console.log(json);
-      fs.writeFile('data/' + country + '.json', JSON.stringify(json, null, 2), function(err) {
-        console.log("Mission succesful");
-        process.exit(0);
-      })
-
+      console.log(json.iTotalRecords);
+      round = Math.ceil(json.iTotalRecords / 500);
+      console.log(round);
+      // process.exit(0);
     }
   })
+
+  for(var i = 0; i < rounds; i++){
+    var page = i * 500;
+    url = "http://www.metal-archives.com/browse/ajax-country/c/" + country + "/json/1?sEcho=1&iDisplayStart=" + page.toString();
+        // console.log(json);
+        fs.writeFile('data/' + country + '.json', JSON.stringify(json, null, 2), function(err) {
+          console.log("Mission succesful");
+        })
+  }
 })
 
 app.listen('8081')
